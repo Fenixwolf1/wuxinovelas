dbc482ab8aa7b0812bc760cea2b63e713311a8f6
Guido <40782641+GuidoValada@users.noreply.github.com>
2020-05-21 17:18:24 -0300

    Update settings.py

--- wuxi/settings.py
+++ wuxi/settings.py
@@ -23,7 +23,7 @@ MEDIA_URL = "/media/"
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5fvykgmf#u#a(a-pv$xo7=y9d50z&rpwkjnu#y+y5lqtcwo&vz'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
